7d24553c7f89f0bfcd01a3bec7053f3de07776d7
hervis30 <hervisjonat@gmail.com>
2022-04-06 16:49:16 -0500

    funcionalidad de botones

--- js/juego.js
+++ js/juego.js
@@ -3,10 +3,33 @@ var puntosJugadorDos = 0;
 var baraja;
 var validarInicio = true;//comprobar si los puntos son <=21
 
+function nuevoJuego() {
+    window.location.reload();
+}
+function ocultarBotones() {
+    document.getElementById("btnUno").style.display = 'none';
+    document.getElementById("btnDos").style.display = 'none';
+    document.getElementById("btnDetener").style.display = 'none';
+}
+function activarBotones() {
+    document.getElementById("btnUno").style.display = 'block';
+    document.getElementById("btnDos").style.display = 'block';
+    document.getElementById("btnDetener").style.display = 'block';
+
+}
+function ocultarbtnNuevo() {
+    document.getElementById("btnNuevo").style.display = 'none';
+}
+function activarbtnNuevo() {
+    document.getElementById("btnNuevo").style.display = 'block';
+}
+
 window.onload = function () {
     hacerBaraja();
     combinarCartas();
     iniciarJuego();
+    activarBotones();
+    ocultarbtnNuevo();
 }
 //se sacan las cartas con sus nombres, aun sin valor
 function hacerBaraja() {
@@ -32,11 +55,10 @@ function combinarCartas() {
 }
 
 function iniciarJuego() {
-
-
     document.getElementById("btnUno").addEventListener("click", nuevaCartaUno);
     document.getElementById("btnDos").addEventListener("click", nuevaCartaDos);
     document.getElementById("btnDetener").addEventListener("click", detener);
+    document.getElementById("btnNuevo").addEventListener("click", nuevoJuego);
 }
 //console.log(puntosJugadorUno);
 
@@ -54,6 +76,7 @@ function nuevaCartaUno() {
 
     if (puntosJugadorUno > 21 || puntosJugadorDos > 21) {
         validarInicio = false;
+        alert('Uno de los jugadores ha superado los 21 Pts. Presione el boton "DETENER"')
     }
 
 }
@@ -71,12 +94,13 @@ function nuevaCartaDos() {
 
     if (puntosJugadorUno > 21 || puntosJugadorDos > 21) {
         validarInicio = false;
+        alert('Uno de los jugadores ha superado los 21 Pts. Presione el boton "DETENER"')
     }
 }
 
 function detener() {
-
-
+    activarbtnNuevo();
+    ocultarBotones();
     validarInicio = false;
 
     let mensaje = "";
@@ -106,7 +130,13 @@ function getValue(carta) {
         if (isNaN(value)) {
             if (value == "A") {//A J Q K validando el valor de A
                 let valorA = prompt('Has sacado la Carta "A", selecciona "11","10" o "1"')
-                return parseInt(valorA);
+                if (valorA == 1 || valorA == 10 || valorA == 11) {
+                    return parseInt(valorA);
+                } else {
+                    alert("Por favor ingresa un valor valido para la carta A  ")
+                    return false;
+                }
+
             }
             return 10;
         }
